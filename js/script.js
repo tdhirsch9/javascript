24ee0b1f4cd7c3c7b490182985c48b8e696635f0
Ted Hirsch <tdhirsch9@gmail.com>
2024-06-19 16:18:24 -0600

    css/style.css

---
+++ js/script.js
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <link rel="shortcut icon" href="#">
+  <meta charset="UTF-8">
+  <title>Page Title</title>
+</head>
+<body>
+  <script>
+
+
+
+let add7 = (a) => {
+  return a + 7
+}
+console.log(add7(2))
+
+let multiply = (a, b) => {
+  return a + b
+}
+
+console.log(multiply(2, 2))
+
+let capitalize = (str) => {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+console.log(capitalize("howdy"))
+
+
+let lastLetter = (end) => {
+  return end.slice(-1)
+}
+
+
+console.log(end("howdy"))
+
+
+
+  </script>
+</body>
+</html>
